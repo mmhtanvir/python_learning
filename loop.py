05d004f69db2e928b1c2c6c5beb994c5509feed6
mmhtanvir <mmhtanvir1@gmail.com>
2023-10-05 19:19:18 +0600

    prints triangle

--- loop.py
+++ loop.py
@@ -2,7 +2,6 @@ for i in range(1, 101):
     print(i)
 
 
-
 # 1-100 even number
 for i in range(2 , 101, 2):
     print(i)
@@ -10,3 +9,14 @@ for i in range(2 , 101, 2):
 # 1-100 odd number
 for i in range(1 , 101, 2):
     print(i)
+
+
+#triangle
+
+num_rows = 10
+
+for i in range(num_rows):
+    for i in range(i + 1):
+        print("*", end="")
+
+    print()
