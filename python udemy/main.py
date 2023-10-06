e696e44bb4935854fa2164555effb54111561f8d
mmhtanvir <mmhtanvir1@gmail.com>
2023-10-06 18:16:02 +0600

    second commit

--- python udemy/main.py
+++ python udemy/main.py
@@ -13,3 +13,12 @@ x = 20
 y = 20
 
 print(f"the sum of X & y is {x+y}")
+
+
+
+#task: 1. Write a script that adds 15 and 30 and divides the sum by 2. Print out the answer.
+x = 15
+y = 30
+z = x+y
+print(z/2)
+print(z/2)
