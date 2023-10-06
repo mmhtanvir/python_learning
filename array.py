bfae68e36c5e0e9fb1eb9ff54949655527075817
mmhtanvir <mmhtanvir1@gmail.com>
2023-10-06 17:43:54 +0600

    initial

--- array.py
+++ array.py
@@ -3,3 +3,21 @@ fruits.append("jackfruit")
 fruits.pop(1)
 
 print(fruits)
+
+#alphabet
+fruits = ['apple', 'banana', 'mango']
+letters = [chr(i) for i in range(ord('a'), ord('z')+1)]
+
+combined_array = fruits + letters
+
+for i in combined_array:
+    print(i)
+
+#number
+fruits = ['apple', 'banana', 'mango']
+numbers = list(range(1, 11))
+
+combined_array = fruits + numbers
+
+for i in combined_array:
+    print(i)
