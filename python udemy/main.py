9acea6af23f47b464f18673489f248a917adf566
mmhtanvir <mmhtanvir1@gmail.com>
2023-10-06 18:05:14 +0600

    first commit

---
+++ python udemy/main.py
@@ -0,0 +1,15 @@
+sentence = "%s is %d years old"
+
+print(sentence % ("tanvir", 18))
+
+
+#format strings
+
+name = "tanvir"
+print(f"hello,{name}")
+
+
+x = 20
+y = 20
+
+print(f"the sum of X & y is {x+y}")
