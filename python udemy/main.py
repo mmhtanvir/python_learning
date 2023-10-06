9aaed8339cbab029a9ea110b5cb15436c47eaff2
mmhtanvir <mmhtanvir1@gmail.com>
2023-10-06 18:20:12 +0600

    third

--- python udemy/main.py
+++ python udemy/main.py
@@ -16,9 +16,21 @@ print(f"the sum of X & y is {x+y}")
 
 
 
-#task: 1. Write a script that adds 15 and 30 and divides the sum by 2. Print out the answer.
+#task:
+
+#1. Write a script that adds 15 and 30 and divides the sum by 2. Print out the answer.
+
 x = 15
 y = 30
 z = x+y
 print(z/2)
 print(z/2)
+
+
+#2. Initialize two variables and use arithmetic operators to add, subtract, multiply, divide, and find the remainder.
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
