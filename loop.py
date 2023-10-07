aa557107208decb0cf598d4a164768f0fa185355
mmhtanvir <mmhtanvir1@gmail.com>
2023-10-07 23:05:21 +0600

    uncommented everything

--- loop.py
+++ loop.py
@@ -1,37 +1,31 @@
-# for i in range(1, 101):
-#     print(i)
+for i in range(1, 101):
+    print(i)
 
 
-# # 1-100 even number
-# for i in range(2 , 101, 2):
-#     print(i)
+# 1-100 even number
+for i in range(2 , 101, 2):
+    print(i)
 
-# # 1-100 odd number
-# for i in range(1 , 101, 2):
-#     print(i)
-
-
-
-# #capital alphabet
-# for letter in range(ord('A'), ord('Z')+1):
-#     print(chr(letter))
-
-
-# #small alphabet
-# for letter in range(ord('a'), ord('z')+1):
-#     print(chr(letter))
-# #triangle
-
-# num_rows = 10
-
-# for i in range(num_rows):
-#     for i in range(i + 1):
-#         print("*", end="")
-
-#     print()     
+# 1-100 odd number
+for i in range(1 , 101, 2):
+    print(i)
 
 
 
+#capital alphabet
+for letter in range(ord('A'), ord('Z')+1):
+    print(chr(letter))
 
 
-print(chr('z')) 
+#small alphabet
+for letter in range(ord('a'), ord('z')+1):
+    print(chr(letter))
+#triangle
+
+num_rows = 10
+
+for i in range(num_rows):
+    for i in range(i + 1):
+        print("*", end="")
+
+    print()     
